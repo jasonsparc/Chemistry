35de1c44a68e417c6998b7281c157138f77d300d
jason <jason.sparc.7@gmail.com>
2016-07-11 20:23:22 +0800

    Add Flask and related components

---
+++ library/src/main/java/io/jasonsparc/chemistry/Flask.java
@@ -0,0 +1,34 @@
+package io.jasonsparc.chemistry;
+
+import android.support.annotation.AnyRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Defines the mapping between an item view type and a view holder. Acts as an object
+ * representation of a view type as well as the factory for its view holder.
+ * <p>
+ * Created by jason on 07/07/2016.
+ *
+ * @see RecyclerView.Adapter#getItemViewType(int)
+ * @see RecyclerView.Adapter#onCreateViewHolder(ViewGroup, int)
+ */
+public interface Flask<VH extends RecyclerView.ViewHolder> {
+
+	/**
+	 * Provides the view type of the items associated with this {@link Flask}. The value returned
+	 * here must be a resource identifier (i.e. a layout resource, a string resource, an id
+	 * resource, etc.) to ensure its uniqueness. The returned resource identifier can either belong
+	 * to the application or a framework-specific resource identifier.
+	 * <p>
+	 * Both {@link Chemistry} and {@link ChemistryAdapter} are designed to throw an exception if
+	 * the item view type is not a valid resource identifier.
+	 *
+	 * @see RecyclerView.Adapter#getItemViewType(int)
+	 */
+	@ViewType
+	@AnyRes
+	int getViewType();
+
+	VH createViewHolder(ViewGroup parent);
+}
