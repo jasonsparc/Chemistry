32b519cf5e47709e4cff368a76bb5a85701218de
jason <jason.sparc.7@gmail.com>
2016-07-11 20:24:14 +0800

    Add utility for inflating layouts

---
+++ library/src/main/java/io/jasonsparc/chemistry/internal/util/InflateUtils.java
@@ -0,0 +1,22 @@
+package io.jasonsparc.chemistry.internal.util;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jason on 07/07/2016.
+ */
+public class InflateUtils {
+
+	public static View inflate(@NonNull Context context, @LayoutRes int layoutRes, @NonNull ViewGroup parent) {
+		return LayoutInflater.from(context).inflate(layoutRes, parent, false);
+	}
+
+	public static View inflate(@LayoutRes int layoutRes, @NonNull ViewGroup parent) {
+		return inflate(parent.getContext(), layoutRes, parent);
+	}
+}
