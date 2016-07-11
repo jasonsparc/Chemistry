419b9b4d7d2bc5fb19f5020b6e2570cd8b7af9f1
jason <jason.sparc.7@gmail.com>
2016-07-11 20:34:15 +0800

    Add reflective VhFactory

---
+++ library/src/main/java/io/jasonsparc/chemistry/internal/flasks/ReflectiveVhFactory.java
@@ -0,0 +1,43 @@
+package io.jasonsparc.chemistry.internal.flasks;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+
+import java.lang.reflect.Constructor;
+
+import io.jasonsparc.chemistry.VhFactory;
+
+/**
+ * Created by jason on 07/07/2016.
+ */
+public class ReflectiveVhFactory<VH extends ViewHolder> implements VhFactory<VH> {
+	static final Class<?>[] sConstructorSignature = {View.class};
+
+	@NonNull final Constructor<? extends VH> constructor;
+
+	public ReflectiveVhFactory(@NonNull Class<? extends VH> cls) {
+		this.constructor = getConstructor(cls);
+	}
+
+	@Override
+	public VH createViewHolder(View itemView) {
+		try {
+			return constructor.newInstance(itemView);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@NonNull
+	static <VH> Constructor<? extends VH> getConstructor(@NonNull Class<? extends VH> cls) {
+		try {
+			Constructor<? extends VH> constructor = cls.getDeclaredConstructor(sConstructorSignature);
+			// the constructor might not be public
+			constructor.setAccessible(true);
+			return constructor;
+		} catch (NoSuchMethodException e) {
+			throw new IllegalArgumentException("The specified `ViewHolder` class must have a constructor receiving an item view.", e);
+		}
+	}
+}
