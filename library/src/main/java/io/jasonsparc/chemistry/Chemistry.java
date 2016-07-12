5a9e1fd617c3ae2a8aa181b88a4bc694bd0595a0
jason <jason.sparc.7@gmail.com>
2016-07-12 12:08:34 +0800

    Add stub for Flask selection and related classes

--- library/src/main/java/io/jasonsparc/chemistry/Chemistry.java
+++ library/src/main/java/io/jasonsparc/chemistry/Chemistry.java
@@ -11,5 +11,17 @@ import android.support.v7.widget.RecyclerView.ViewHolder;
  */
 public abstract class Chemistry {
 
+	/**
+	 * TODO Improve Docs
+	 * <br>- associates a flask to the specified item class
+	 * <br>- a null selector deletes the association
+	 *
+	 * @param itemClass
+	 * @param flaskSelector
+	 * @param <Item>
+	 * @return
+	 */
+	public abstract <Item> Chemistry flask(@Nullable Class<? extends Item> itemClass, @Nullable FlaskSelector<? super Item> flaskSelector);
+
 	public abstract <Item, VH extends ViewHolder> Chemistry bind(@Nullable Class<? extends Item> itemClass, @NonNull BindPredicate<? extends VH> bindPredicate, @Nullable ItemBinder<? super Item, ? super VH> itemBinder);
 }
