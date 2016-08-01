f136f75440259fe2caeb9f05056943eba54167e8
jason <jason.sparc.7@gmail.com>
2016-08-01 19:49:25 +0800

    Clarify BindPredicate docs

--- library/src/main/java/io/jasonsparc/chemistry/BindPredicate.java
+++ library/src/main/java/io/jasonsparc/chemistry/BindPredicate.java
@@ -14,7 +14,8 @@ public interface BindPredicate<VH extends ViewHolder> {
 	 * TODO Improve Docs
 	 * <p>
 	 * CONTRACT: this method should return {@code false} if the given {@link ViewHolder ViewHolder}
-	 * class does not match the type parameter specified by this {@link BindPredicate} instance.
+	 * class does not match the type parameter specified by this {@link BindPredicate} instance via
+	 * an upcast.
 	 *
 	 * @param vhClass
 	 * @param flask
