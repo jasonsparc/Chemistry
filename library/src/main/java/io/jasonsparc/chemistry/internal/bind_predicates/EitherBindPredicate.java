238c64a82296b3051ab9fbb2ef999d36d2c63d67
jason <jason.sparc.7@gmail.com>
2016-08-04 16:56:26 +0800

    Add ability to combine BindPredicate's

---
+++ library/src/main/java/io/jasonsparc/chemistry/internal/bind_predicates/EitherBindPredicate.java
@@ -0,0 +1,25 @@
+package io.jasonsparc.chemistry.internal.bind_predicates;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+
+import io.jasonsparc.chemistry.BindPredicate;
+import io.jasonsparc.chemistry.Flask;
+
+/**
+ * Created by jason on 02/08/2016.
+ */
+public final class EitherBindPredicate<VH extends ViewHolder> implements BindPredicate<VH> {
+	@NonNull final BindPredicate<? extends VH> first;
+	@NonNull final BindPredicate<? extends VH> second;
+
+	public EitherBindPredicate(@NonNull BindPredicate<? extends VH> first, @NonNull BindPredicate<? extends VH> second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	@Override
+	public boolean checkBind(@NonNull Class<? extends ViewHolder> vhClass, @NonNull Flask<?> flask) {
+		return first.checkBind(vhClass, flask) || second.checkBind(vhClass, flask);
+	}
+}
--- library/src/main/java/io/jasonsparc/chemistry/util/BindPredicates.java
+++ library/src/main/java/io/jasonsparc/chemistry/util/BindPredicates.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 
 import io.jasonsparc.chemistry.BindPredicate;
 import io.jasonsparc.chemistry.Flask;
+import io.jasonsparc.chemistry.internal.bind_predicates.EitherBindPredicate;
+import io.jasonsparc.chemistry.internal.bind_predicates.FusedBindPredicate;
 import io.jasonsparc.chemistry.internal.bind_predicates.VhClassArrayBindPredicate;
 import io.jasonsparc.chemistry.internal.bind_predicates.VhClassBindPredicate;
 
@@ -40,5 +42,69 @@ public class BindPredicates {
 	@SuppressWarnings("unchecked")
 	public static <VH extends ViewHolder> BindPredicate<VH> of(@NonNull Collection<Class<? extends VH>> vhClasses) {
 		return new VhClassArrayBindPredicate<>(vhClasses.toArray(new Class[vhClasses.size()]));
+	}
+
+	// Logical Operations
+
+	public static <VH extends ViewHolder> BindPredicate<VH> fuse(@NonNull BindPredicate<? extends VH> first, @NonNull BindPredicate<? extends VH> second) {
+		return new FusedBindPredicate<>(first, second);
+	}
+
+	@SuppressWarnings("unchecked")
+	@SafeVarargs
+	public static <VH extends ViewHolder> BindPredicate<VH> fuse(@NonNull BindPredicate<? extends VH>... bindPredicates) {
+		int l = bindPredicates.length;
+		switch (l) {
+			case 0:
+				throw new IllegalArgumentException("length == 0");
+			case 1:
+				return (BindPredicate<VH>) bindPredicates[0];
+			case 2:
+				return fuse(bindPredicates[0], bindPredicates[1]);
+		}
+		BindPredicate<VH> r = (BindPredicate<VH>) bindPredicates[--l];
+		do r = fuse(bindPredicates[--l], r); while (l > 0);
+		return r;
+	}
+
+	public static <VH extends ViewHolder> BindPredicate<VH> fuse(Collection<? extends BindPredicate<? extends VH>> bindPredicates) {
+		return fuse(array(bindPredicates));
+	}
+
+	public static <VH extends ViewHolder> BindPredicate<VH> either(@NonNull BindPredicate<? extends VH> first, @NonNull BindPredicate<? extends VH> second) {
+		return new EitherBindPredicate<>(first, second);
+	}
+
+	@SuppressWarnings("unchecked")
+	@SafeVarargs
+	public static <VH extends ViewHolder> BindPredicate<VH> either(@NonNull BindPredicate<? extends VH>... bindPredicates) {
+		int l = bindPredicates.length;
+		switch (l) {
+			case 0:
+				throw new IllegalArgumentException("length == 0");
+			case 1:
+				return (BindPredicate<VH>) bindPredicates[0];
+			case 2:
+				return either(bindPredicates[0], bindPredicates[1]);
+		}
+		BindPredicate<VH> r = (BindPredicate<VH>) bindPredicates[--l];
+		do r = either(bindPredicates[--l], r); while (l > 0);
+		return r;
+	}
+
+	public static <VH extends ViewHolder> BindPredicate<VH> either(Collection<? extends BindPredicate<? extends VH>> bindPredicates) {
+		return either(array(bindPredicates));
+	}
+
+	// Utilities
+
+	@SafeVarargs
+	public static <P extends BindPredicate<? extends ViewHolder>> P[] array(@NonNull P... bindPredicates) {
+		return bindPredicates;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <VH extends ViewHolder> BindPredicate<? extends VH>[] array(@NonNull Collection<? extends BindPredicate<? extends VH>> bindPredicates) {
+		return bindPredicates.toArray(new BindPredicate[bindPredicates.size()]);
 	}
 }
