0a42ca3cae91b65642407c517ae8f570c0ad116a
jason <jason.sparc.7@gmail.com>
2016-08-19 06:46:55 +0800

    Add NullChemistry utility

---
+++ library/src/main/java/io/jasonsparc/chemistry/internal/NullChemistry.java
@@ -0,0 +1,41 @@
+package io.jasonsparc.chemistry.internal;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.ViewGroup;
+
+import io.jasonsparc.chemistry.BasicChemistry;
+import io.jasonsparc.chemistry.VhInitializer;
+
+/**
+ * Created by Jason on 19/08/2016.
+ */
+public final class NullChemistry<Item, VH extends ViewHolder> extends BasicChemistry<Item, VH> implements VhInitializer<VH> {
+	private static final NullChemistry INSTANCE = new NullChemistry<>();
+
+	private NullChemistry() { }
+
+	@SuppressWarnings("unchecked")
+	public static <Item, VH extends ViewHolder> NullChemistry<Item, VH> get() {
+		return INSTANCE;
+	}
+
+	@Override
+	public int getViewType() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public VH createViewHolder(ViewGroup parent) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void initViewHolder(VH holder) {
+		// Do nothing.
+	}
+
+	@Override
+	public void bindViewHolder(ViewHolder holder, Object o) {
+		// Do nothing.
+	}
+}
