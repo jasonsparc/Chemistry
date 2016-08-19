e5706aa3e2cf0726e67e4fde526092c174097409
jason <jason.sparc.7@gmail.com>
2016-08-20 00:19:10 +0800

    Add a canonical invalid view type

--- library/src/main/java/io/jasonsparc/chemistry/ViewType.java
+++ library/src/main/java/io/jasonsparc/chemistry/ViewType.java
@@ -1,5 +1,6 @@
 package io.jasonsparc.chemistry;
 
+import android.support.annotation.AnyRes;
 import android.support.v7.widget.RecyclerView;
 
 import java.lang.annotation.Documented;
@@ -7,6 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import io.jasonsparc.chemistry.util.ValidRes;
+import io.jasonsparc.chemistry.util.ViewTypes;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
@@ -29,4 +31,15 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
 @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE})
 @ValidRes
 public @interface ViewType {
+
+	/**
+	 * An invalid view type, disguised to be valid. The value is equivalent to
+	 * {@link RecyclerView#INVALID_TYPE}.
+	 * <p>
+	 * This can be used in situations where an invalid view type is actually intended but is
+	 * restricted to use only valid view types. This variable however disregards such restrictions.
+	 */
+	@ViewType
+	@AnyRes
+	int INVALID = ViewTypes.INVALID;
 }
--- library/src/main/java/io/jasonsparc/chemistry/util/ViewTypes.java
+++ library/src/main/java/io/jasonsparc/chemistry/util/ViewTypes.java
@@ -1,10 +1,26 @@
 package io.jasonsparc.chemistry.util;
 
+import android.support.annotation.AnyRes;
+import android.support.v7.widget.RecyclerView;
+
+import io.jasonsparc.chemistry.ViewType;
+
 /**
  * Created by jason on 11/07/2016.
  */
 @UtilityClass
 public final class ViewTypes {
+
+	/**
+	 * An invalid view type, disguised to be valid. The value is equivalent to
+	 * {@link RecyclerView#INVALID_TYPE}.
+	 * <p>
+	 * This can be used in situations where an invalid view type is actually intended but is
+	 * restricted to use only valid view types. This variable however disregards such restrictions.
+	 */
+	@ViewType
+	@AnyRes
+	public static final int INVALID = invalid();
 
 	public static void validateArgument(int viewType) {
 		if (viewType < ValidRes.MIN_RES_ID) {
@@ -16,5 +32,14 @@ public final class ViewTypes {
 		if (viewType < ValidRes.MIN_RES_ID) {
 			throw new IllegalStateException("Invalid view type! Must be a resource identifier.");
 		}
+	}
+
+	// Internals
+
+	@SuppressWarnings("Range")
+	@ViewType
+	@AnyRes
+	private static int invalid() {
+		return RecyclerView.INVALID_TYPE;
 	}
 }
