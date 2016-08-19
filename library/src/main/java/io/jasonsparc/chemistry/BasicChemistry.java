7d9289fd4d23de68918a19d1693be21d4313a26b
jason <jason.sparc.7@gmail.com>
2016-08-19 09:12:21 +0800

    Implement composite pattern for Chemistry

--- library/src/main/java/io/jasonsparc/chemistry/BasicChemistry.java
+++ library/src/main/java/io/jasonsparc/chemistry/BasicChemistry.java
@@ -1,8 +1,15 @@
 package io.jasonsparc.chemistry;
 
 import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import io.jasonsparc.chemistry.util.IdSelectors;
+import io.jasonsparc.chemistry.util.ItemBinders;
+import io.jasonsparc.chemistry.util.VhFactories;
 
 /**
  * Created by Jason on 19/08/2016.
@@ -33,4 +40,137 @@ public abstract class BasicChemistry<Item, VH extends ViewHolder> extends Chemis
 
 	@Override
 	public abstract void bindViewHolder(VH holder, Item item);
+
+
+	// Utilities
+
+	public <RI extends Item> Boiler<RI, VH> compose() {
+		return make(this);
+	}
+
+	public <RI extends Item> Boiler<RI, VH> compose(@ViewType @AnyRes int viewType) {
+		return make(viewType, this);
+	}
+
+
+	public interface Transformer<Item, VH extends ViewHolder> {
+
+		void apply(Boiler<? extends Item, ? extends VH> boiler);
+	}
+
+	public <RI extends Item> Boiler<RI, VH> compose(@NonNull Transformer<? super RI, ? super VH> transformer) {
+		return this.<RI>compose().compose(transformer);
+	}
+
+
+	// Boiler implementation
+
+	public static class Boiler<Item, VH extends ViewHolder> {
+
+		// Composition operations
+
+		public BasicChemistry<Item, VH> boil() {
+			return new CompositeImpl<>(this);
+		}
+
+		public Boiler<Item, VH> compose(@NonNull Transformer<? super Item, ? super VH> transformer) {
+			transformer.apply(this);
+			return this;
+		}
+
+
+		public Boiler<Item, VH> setItemIds(@NonNull IdSelector<? super Item> idSelector) {
+			this.idSelector = idSelector;
+			return this;
+		}
+
+		public Boiler<Item, VH> setViewType(@ViewType @AnyRes int viewType) {
+			this.viewType = viewType;
+			return this;
+		}
+
+
+		public Boiler<Item, VH> addInit(@NonNull VhInitializer<? super VH> vhInitializer) {
+			return add(vhInitializer);
+		}
+
+		public Boiler<Item, VH> addBinder(@NonNull ItemBinder<? super Item, ? super VH> itemBinder) {
+			return add(itemBinder);
+		}
+
+
+		public Boiler<Item, VH> add(@NonNull VhInitializer<? super VH> vhInitializer) {
+			vhInitializers.add(vhInitializer);
+			return this;
+		}
+
+		public Boiler<Item, VH> add(@NonNull ItemBinder<? super Item, ? super VH> itemBinder) {
+			itemBinders.add(itemBinder);
+			return this;
+		}
+
+
+		// Internals
+
+		@NonNull final VhFactory<? extends VH> vhFactory;
+		final ArrayList<VhInitializer<? super VH>> vhInitializers;
+		final ArrayList<ItemBinder<? super Item, ? super VH>> itemBinders;
+
+		@NonNull IdSelector<? super Item> idSelector = IdSelectors.empty();
+		@ViewType @AnyRes int viewType;
+
+		Boiler(@ViewType @AnyRes int viewType, @NonNull VhFactory<? extends VH> vhFactory) {
+			this.vhFactory = vhFactory;
+			this.vhInitializers = new ArrayList<>(4);
+			this.itemBinders = new ArrayList<>(4);
+			this.viewType = viewType;
+		}
+
+		@SuppressWarnings("unchecked")
+		Boiler(@NonNull BasicChemistry<? super Item, VH> base) {
+			vhInitializers = new ArrayList<>(4);
+			itemBinders = new ArrayList<>(4);
+
+			if (base instanceof CompositeImpl<?, ?>) {
+				CompositeImpl<? super Item, VH> composite = (CompositeImpl) base;
+				vhFactory = composite.vhFactory;
+				itemBinders.add(base);
+				viewType = composite.viewType;
+			} else {
+				vhFactory = base;
+				itemBinders.add(base);
+				viewType = base.getViewType();
+			}
+		}
+	}
+
+	static final class CompositeImpl<Item, VH extends ViewHolder> extends BasicChemistry<Item, VH> {
+		@NonNull final VhFactory<? extends VH> vhFactory;
+		@NonNull final ItemBinder<? super Item, ? super VH> itemBinder;
+
+		@NonNull final IdSelector<? super Item> idSelector;
+		@ViewType @AnyRes final int viewType;
+
+		CompositeImpl(@NonNull BasicChemistry.Boiler<Item, VH> boiler) {
+			this.vhFactory = VhFactories.make(boiler.vhFactory, boiler.vhInitializers);
+			this.itemBinder = ItemBinders.make(boiler.itemBinders);
+			this.idSelector = boiler.idSelector;
+			this.viewType = boiler.viewType;
+		}
+
+		@Override
+		public int getViewType() {
+			return viewType;
+		}
+
+		@Override
+		public VH createViewHolder(ViewGroup parent) {
+			return vhFactory.createViewHolder(parent);
+		}
+
+		@Override
+		public void bindViewHolder(VH holder, Item item) {
+			itemBinder.bindViewHolder(holder, item);
+		}
+	}
 }
--- library/src/main/java/io/jasonsparc/chemistry/Chemistry.java
+++ library/src/main/java/io/jasonsparc/chemistry/Chemistry.java
@@ -1,8 +1,16 @@
 package io.jasonsparc.chemistry;
 
 import android.support.annotation.AnyRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.jasonsparc.chemistry.BasicChemistry.Boiler;
+import io.jasonsparc.chemistry.util.InflateUtils;
+import io.jasonsparc.chemistry.util.VhFactories;
 
 /**
  * TODO Docs
@@ -22,4 +30,87 @@ public abstract class Chemistry<Item, VH extends ViewHolder> implements IdSelect
 	public abstract VhFactory<? extends VH> getVhFactory(Item item);
 
 	public abstract ItemBinder<? super Item, ? super VH> getItemBinder(Item item);
+
+	// Utilities
+
+	public static View inflate(@NonNull ViewGroup parent, @LayoutRes int layoutRes) {
+		return InflateUtils.inflate(parent, layoutRes);
+	}
+
+	// Factories
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType @AnyRes int viewType, @NonNull VhFactory<? extends VH> vhFactory) {
+		return new BasicChemistry.Boiler<>(viewType, vhFactory);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType @AnyRes int viewType, @NonNull ViewFactory viewFactory, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return new BasicChemistry.Boiler<>(viewType, VhFactories.make(viewFactory, itemVhFactory));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType @AnyRes int viewType, @NonNull ViewFactory viewFactory, @NonNull Class<? extends VH> vhClass) {
+		return new BasicChemistry.Boiler<>(viewType, VhFactories.make(viewFactory, vhClass));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType @AnyRes int viewType, @LayoutRes int itemLayout, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return new BasicChemistry.Boiler<>(viewType, VhFactories.make(itemLayout, itemVhFactory));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType @AnyRes int viewType, @LayoutRes int itemLayout, @NonNull Class<? extends VH> vhClass) {
+		return new BasicChemistry.Boiler<>(viewType, VhFactories.make(itemLayout, vhClass));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@LayoutRes int itemLayout, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return new BasicChemistry.Boiler<>(itemLayout, VhFactories.make(itemLayout, itemVhFactory));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@LayoutRes int itemLayout, @NonNull Class<? extends VH> vhClass) {
+		return new BasicChemistry.Boiler<>(itemLayout, VhFactories.make(itemLayout, vhClass));
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull BasicChemistry<? super Item, VH> base) {
+		return new Boiler<>(base);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@ViewType int viewType, @NonNull BasicChemistry<? super Item, VH> base) {
+		final Boiler<Item, VH> boiler = new Boiler<>(base);
+		return boiler.setViewType(viewType);
+	}
+
+	// Factories - with item class specifiers
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType @AnyRes int viewType, @NonNull VhFactory<? extends VH> vhFactory) {
+		return make(viewType, vhFactory);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType @AnyRes int viewType, @NonNull ViewFactory viewFactory, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return make(viewType, viewFactory, itemVhFactory);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType @AnyRes int viewType, @NonNull ViewFactory viewFactory, @NonNull Class<? extends VH> vhClass) {
+		return make(viewType, viewFactory, vhClass);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType @AnyRes int viewType, @LayoutRes int itemLayout, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return make(viewType, itemLayout, itemVhFactory);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType @AnyRes int viewType, @LayoutRes int itemLayout, @NonNull Class<? extends VH> vhClass) {
+		return make(viewType, itemLayout, vhClass);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @LayoutRes int itemLayout, @NonNull ItemVhFactory<? extends VH> itemVhFactory) {
+		return make(itemLayout, itemVhFactory);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @LayoutRes int itemLayout, @NonNull Class<? extends VH> vhClass) {
+		return make(itemLayout, vhClass);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @NonNull BasicChemistry<? super Item, VH> base) {
+		return make(base);
+	}
+
+	public static <Item, VH extends ViewHolder> BasicChemistry.Boiler<Item, VH> make(@NonNull Class<? extends Item> itemClass, @ViewType int viewType, @NonNull BasicChemistry<? super Item, VH> base) {
+		return make(viewType, base);
+	}
 }
